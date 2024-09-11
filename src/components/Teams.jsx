f85d33727539e28acc02743edc7548ce1280e37e
jcfranco <migranyami@gmail.com>
2024-09-11 09:43:45 -0500

    component & JSON updated

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -4,19 +4,30 @@ import { useState, useEffect } from "react";
 
 export default function Teams() {
   const [tabla, setTabla] = useState([]);
+  const [indexShow, setNewIndex] = useState(0);
 
   useEffect(() => {
     const newOrder = orderPosition(data);
-    setTabla(newOrder);
-  }, []);
+    setTabla(showResult(newOrder, indexShow));
+
+    const interval = setInterval(() => {
+      setNewIndex((prevIndex) => {
+        const newIndex = prevIndex === 0 ? 1 : 0;
+        setTabla(showResult(newOrder, newIndex));
+        return newIndex;
+      });
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [indexShow]);
 
   return (
     <div className="containerTable">
       {tabla.map((obj, index) => (
-        <div className="team" key={index}> 
+        <div className="team" key={index}>
           <div className="posLogo">
-            <p>{index + 1}</p>
-            <img src={obj.shield} alt={obj.name}/>
+            <p>{obj.tablePosition}</p>
+            <img src={obj.shield} alt={obj.name} />
           </div>
           <h5>{obj.name}</h5>
           <div className="info">
@@ -30,7 +41,9 @@ export default function Teams() {
           </div>
           <div className="puntos">
             <span>
-              <b>PUNTOS: </b>{obj.PUNTOS}</span>
+              <b>PUNTOS: </b>
+              {obj.PUNTOS}
+            </span>
           </div>
         </div>
       ))}
@@ -39,7 +52,7 @@ export default function Teams() {
 }
 
 function orderPosition(equipos) {
-  return equipos.sort((a, b) => {
+  const sortedEquipos =  equipos.sort((a, b) => {
     if (b.PUNTOS !== a.PUNTOS) {
       return b.PUNTOS - a.PUNTOS; // Ordenar por puntos
     } else if (b.GD !== a.GD) {
@@ -51,5 +64,21 @@ function orderPosition(equipos) {
     } else {
       return 0; // Si todos los criterios son iguales
     }
-  });
+  }
+);
+return sortedEquipos.map((equipo, index) => ({
+    ...equipo, 
+    tablePosition: index + 1, 
+  }));
+}
+
+function showResult(listEquipos, indexShow = 0) {
+  let newResults = listEquipos.slice(0, 10);
+  let newResults2 = listEquipos.slice(10);
+
+  if (indexShow === 0) {
+    return newResults;
+  } else if (indexShow === 1) {
+    return newResults2;
+  }
 }
